aa4ba5888d730c26a7aec8bcc80a3978aceb5948
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-01 10:57:43 -0600

    adds update feature

--- pymongo.py
+++ pymongo.py
@@ -56,6 +56,17 @@ mydoc = mycol.find().sort("name", -1)
 for x in mydoc:
     print(x)
 # sorting the results in descending order
+myquery = {"address": "Valley 345"}
+newvalues = {"$set": {"address": "Canyon 123"}}
+mycol.update_one(myquery, newvalues)
+for x in mycol.find():
+    print(x)
+# updates collection
+myquery = {"address": {"$regex": "^S"}}
+newvalues = {"$set": {"name": "Minnie"}}
+x = mycol.update_many(myquery, newvalues)
+print(x.modified_count, "documents updated.")
+# updates all documents where the address starts with the letter "S"
 myquery = {"address": "Mountain 21"}
 mycol.delete_one(myquery)
 myquery = {"address": {"$regex": "^S"}}
